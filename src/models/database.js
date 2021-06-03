44b23db15a64ff547aa5cbafde807ed539e03b2a
cristian-cll <78435266+cristian-cll@users.noreply.github.com>
2021-06-03 13:13:54 +0200

    new search and order action. fix bugs

--- src/models/database.js
+++ src/models/database.js
@@ -23,13 +23,13 @@ let photos = [{
     palette: "236,97,39"
 },{
     id: 3,
-    title: "Example3",
+    title: "AAAAA",
     image: "https://i.blogs.es/594843/chrome/450_1000.jpg",
     date: "2022-05-28",
     palette: "12,92,155"
 },{ 
     id: 4,
-    title: "Example4",
+    title: "ZZZZ",
     image: "https://www.teknofilo.com/wp-content/uploads/2020/03/whatsapp-1280x720.jpg",
     date: "2023-05-28",
     palette: "147,219,115"
@@ -37,7 +37,7 @@ let photos = [{
     id: 5,
     title: "Homer",
     image: "https://3.bp.blogspot.com/-JfL1o7oSnKI/VmodObHF9cI/AAAAAAAABLY/nKKRXw0-yiU/s1600/homero_456_336.jpg",
-    date: "2023-05-28",
+    date: "2025-05-28",
     palette: "66,138,212"
 }];
 
@@ -61,7 +61,7 @@ exports.addPhoto = async function(title, image, date){
 };
 
 
-exports.getPhotos = () => photos.sort((a, b) =>{ return new Date(b.date) - new Date(a.date)});
+exports.getPhotos = () => photos;
 //We export the photos array and sort by most recent date. We have to parse the string to a date format.
 
 exports.deletePhoto = function(id){
@@ -77,3 +77,15 @@ exports.editPhoto = function(id, title, image, date){
         }
     });
 };
+
+exports.searchPhotos = function(title) {
+    return photos.filter((photo) => photo.title.toUpperCase().includes(title.toUpperCase()));
+}
+
+exports.orderBy = function(by) {
+    if(by === "title"){
+        return photos.sort((a,b) => (a.title > b.title) ? 1 : ((b.title > a.title) ? -1 : 0))
+    }
+
+    return photos.sort((a,b) =>new Date(b.date) - new Date(a.date));
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,12 +6,11 @@ const database = require("../models/database");
 
 router.get("/", (req, res) => {
     res.render("index", {
-        allphotos: database.getPhotos(),
+        allphotos: database.getPhotos().sort((a,b) =>new Date(b.date) - new Date(a.date)),
         title: "Pic.to.me Gallery",
         page_name: "home"
     });
 });
-
 
 
 router.post("/add", async (req, res)=>{
@@ -20,18 +19,17 @@ router.post("/add", async (req, res)=>{
     const photoUrl = req.body.url; 
     const photoDate = req.body.date; 
     
+
+    //MIRARLO BIEN!!!!!!!
     if(database.checkDuplicateUrl(photoUrl)){
         return res.render("addimage", {
             error: true,
             url: photoUrl
         });
     }
-
     await database.addPhoto(photoTitle, photoUrl, photoDate);
-
     res.redirect("/");
 });
-
 
 
 router.get("/delete/:id", (req, res) => {
@@ -55,14 +53,38 @@ router.post("/edit/:id", (req, res) => {
 
 router.get("/checkphoto/", (req, res) => {
     const url = req.query.photoURL;
-    console.log(url);
-    
+
     let isDuplicateUrl = database.checkDuplicateUrl(url)
 
     res.json({
         error: isDuplicateUrl,
         url: url 
     })
+});
+
+
+router.get("/search/", (req, res) => {
+    const title = req.query.title;
+    const search = database.searchPhotos(title)
+
+    res.render("index", {
+        allphotos: search,
+        title: "Pic.to.me Gallery - Results",
+        page_name: "results"
+    });
+});
+
+
+
+router.get("/order/", (req, res) => {
+    let by = req.query.by;
+    let sortedPhotos = database.orderBy(by);
+
+     res.render("index", {
+        allphotos: sortedPhotos,
+        title: "Pic.to.me Gallery",
+        page_name: "home"
+    }); 
 });
 
 
@@ -74,9 +96,11 @@ router.get("/about", (req, res) => {
 });
 
 
-//Cualquier otro enpoint, tanto si es GET, POST, PUT o DELETE. Si no cae por ningún otro endpoint, devuelve esto.
+//Cualquier otro endpoint, tanto si es GET, POST, PUT o DELETE. Si no cae por ningún otro endpoint, devuelve esto.
 router.use((req, res) => {
-    res.status(404).render("404");
+    res.status(404).render("404", {
+        title: "Pic.to.me Gallery - NOT FOUND",
+    });
 });
 
 
